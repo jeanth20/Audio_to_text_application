c0da72f798ee3ee5eacdb36e38b0f64715a409fe
Jean <64726220+jeanth20@users.noreply.github.com>
2023-06-03 00:46:09 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -41,28 +41,20 @@ with st.sidebar:
     selected = option_menu(
         menu_title="Audio transcription",
         menu_icon="cassette-fill",
-        options=["Transcribe File", "Archive Files", "Transcripts Files"],
-        icons=["music-note-list", "archive-fill", "credit-card-2-front-fill"],
+        options=["Transcribe File", "Audio Archive", "Transcripts Files", "Download Youtube"],
+        icons=["music-note-list", "archive-fill", "credit-card-2-front-fill", "play-btn-fill"],
         default_index=0
     )
 
-    if selected == "Archive":
-        selectedArc = option_menu(
-            None,
-            ["Upload", "Tasks"],
-            icons=["cloud-upload", "list-task"],
-            menu_icon="cast",
-            default_index=0,
-            orientation="horizontal"
-        )
-
 if selected == "Transcribe File":
-    st.write("Transcribe content goes here")
-    
+    # st.write("Transcribe content goes here")
+    files.transcribe_file()
 
-elif selected == "Archive Files":
+elif selected == "Audio Archive":
     files.audio_explorer()
-    
 
 elif selected == "Transcripts Files":
     files.transcripts_explorer()
+    
+elif selected == "Download Youtube":
+    files.download_youtube()
--- dir.py
+++ dir.py
@@ -1,10 +1,12 @@
 import streamlit as st
 import os
-import sounddevice as sd
-import numpy as np
-import streamlit as st
-from pydub import AudioSegment
 import time
+import numpy as np
+import sounddevice as sd
+import speech_recognition as sr
+from pydub import AudioSegment
+from pytube import YouTube
+from moviepy.editor import VideoFileClip
 
 
 def list_directories(directory):
@@ -102,8 +104,182 @@ def audio_explorer():
 
     upload_file(selected_directory_path)
 
+def transcribe_file():
+    st.title("Transcribe File")
+    home_directory = os.getcwd()
+    current_directory = home_directory + "/Audio_Library"
+
+    uploaded_file = st.file_uploader("Upload Audio File", type=["wav", "mp3", "ogg"])
+
+    if uploaded_file is not None:
+        audio_path = os.path.join(current_directory, uploaded_file.name)
+        with open(audio_path, "wb") as file:
+            file.write(uploaded_file.getbuffer())
+
+        audio_format = os.path.splitext(audio_path)[1][1:].lower()
+
+        if audio_format == "ogg":
+            converted_audio_path = convert_to_wav(audio_path)
+            if converted_audio_path is None:
+                st.write("Audio conversion failed.")
+                return
+            audio_path = converted_audio_path
+
+        transcribe_button = st.button("Transcribe")
+
+        if transcribe_button:
+            st.write("Transcribing audio...")
+            transcribed_text = perform_transcription(audio_path)
+
+            if transcribed_text:
+                text_path = home_directory + "/Transcribed_Library"
+                text_path = os.path.splitext(audio_path)[0] + ".txt"
+                with open(text_path, "w") as file:
+                    file.write(transcribed_text)
+                st.write("Transcription completed!")
+                st.download_button("Download Transcription", text_path)
+            else:
+                st.write("Transcription failed.")
 
 
+def convert_to_wav(audio_path):
+    try:
+        audio = AudioSegment.from_file(audio_path)
+        converted_audio_path = os.path.splitext(audio_path)[0] + ".wav"
+        audio.export(converted_audio_path, format="wav")        
+        return converted_audio_path
+    except Exception as e:
+        st.write(f"Error converting audio: {e}")
+        return None
+
+def perform_transcription(audio_path):
+    r = sr.Recognizer()
+
+    with sr.AudioFile(audio_path) as source:
+        audio = r.record(source)
+
+    try:
+        transcribed_text = r.recognize_google(audio, language="en-US")
+        progress_bar = st.progress(0)        
+        for percent_complete in range(0, 101, 20):
+            progress_bar.progress(percent_complete)
+            time.sleep(0.1)
+        progress_bar.empty()
+        st.write("Transcription complete!")
+        return transcribed_text
+    except sr.UnknownValueError:
+        st.write("Unable to transcribe audio. Please try again.")
+    except sr.RequestError:
+        st.write("Could not connect to the speech recognition service. Please try again later.")
+
+
+
+
+def convert_to_wav(audio_file_path):
+    audio = AudioSegment.from_file(audio_file_path)
+    wav_file_path = os.path.splitext(audio_file_path)[0] + '.wav'
+    audio.export(wav_file_path, format='wav')
+    return wav_file_path
+
+def transcribe_file():
+    st.title("Transcribe File")
+    home_directory = os.getcwd()
+    current_directory = home_directory + "/Audio_Library"
+
+    audio_file_path = upload_file(current_directory)
+
+    transcribe_button = st.button("Transcribe")
+
+    if transcribe_button:
+        st.write("Transcribing audio...")
+        
+        # Convert the audio file to WAV format
+        wav_file_path = convert_to_wav(audio_file_path)
+
+        # Perform transcription on the converted WAV file
+        transcribed_text = perform_transcription(wav_file_path)
+
+        st.write("Transcription:")
+        st.write(transcribed_text)
+
+    upload_file(current_directory)
+
+
+# def transcribe_file():
+#     st.title("Transcribe File")
+#     home_directory = os.getcwd()
+#     current_directory = home_directory + "/Audio_Library"
+
+#     uploaded_file = st.file_uploader("Upload Audio File", type=["wav", "mp3"])
+
+#     if uploaded_file is not None:
+#         audio_path = os.path.join(current_directory, uploaded_file.name)
+#         with open(audio_path, "wb") as file:
+#             file.write(uploaded_file.getbuffer())
+
+#         transcribe_button = st.button("Transcribe")
+
+#         if transcribe_button:
+#             st.write("Transcribing audio...")
+
+#             # Perform audio transcription
+#             transcribed_text = perform_transcription(audio_path)
+
+#             if transcribed_text:
+#                 text_path = os.path.splitext(audio_path)[0] + ".txt"
+#                 with open(text_path, "w") as file:
+#                     file.write(transcribed_text)
+#                 st.write("Transcription completed!")
+#                 st.download_button("Download Transcription", text_path)
+#             else:
+#                 st.write("Transcription failed.")
+
+# def perform_transcription(audio_path):
+#     r = sr.Recognizer()
+
+#     with sr.AudioFile(audio_path) as source:
+#         audio = r.record(source)
+
+#     try:
+#         transcribed_text = r.recognize_google(audio, language="en-US")
+#         return transcribed_text
+#     except sr.UnknownValueError:
+#         st.write("Unable to transcribe audio. Please try again.")
+#     except sr.RequestError:
+#         st.write("Could not connect to the speech recognition service. Please try again later.")
+
+
+
+
+
+
+# def transcribe_file():
+#     st.title("Transcribe File")
+#     home_directory = os.getcwd()
+#     current_directory = home_directory + "/Audio_Library"
+
+#     upload_file(current_directory)
+
+#     transcribe = st.button("Transcribe")
+    
+#     if transcribe:
+#         st.write()
+#         import speech_recognition as sr
+
+#         def get_audio():
+#             rObject = sr.Recognizer()
+#             audio = ''
+#             with sr.Microphone() as source:
+#                 print("Listening...")
+#                 audio = rObject.listen(source, phrase_time_limit = 5)
+#             try:
+#                 text = rObject.recognize_google(audio, language ='en-US')
+#                 print("You : ", text)
+#                 return text
+#             except:
+#                 return 0
+
+        
 
 def transcripts_explorer():
     st.title("Transcripts Library")
@@ -121,10 +297,63 @@ def transcripts_explorer():
 
         with open(selected_file_path, "r") as file:
             transcript_content = file.read()
-            st.text_area("Transcript Content", transcript_content)
+            st.text_area("Transcript Content", transcript_content, height=300)
 
         left, center, right = st.columns([1, 1, 3])
         with right:
             download_file(selected_file_path)
 
-    upload_file(selected_directory_path)
+    # upload_file(selected_directory_path)
+
+    
+def download_youtube():
+    # Proofreading Tool
+    # pip install pytube
+    st.title("Download From Youtube")
+    url = st.text_input("Youtube Url")
+    download = st.button("Download")
+    
+    home_directory = os.getcwd()
+    current_directory = home_directory + "/Download_Library"
+
+    if download:
+        # yt = YouTube(url)
+        # audio_stream = yt.streams.filter(only_audio=True).first()
+        # audio_file_path = audio_stream.download()
+        # st.write("Audio downloaded:", current_directory)
+        yt = YouTube(url)
+        audio_stream = yt.streams.filter(only_audio=True).first()
+        audio_file_path = audio_stream.download()
+        print("Audio downloaded:", audio_file_path)
+
+        # Convert the downloaded video to audio
+        audio_file_name = os.path.splitext(audio_file_path)[0] + '.mp3'
+        video_clip = VideoFileClip(audio_file_path)
+        audio_clip = video_clip.audio
+        audio_clip.write_audiofile(audio_file_name)
+        audio_clip.close()
+        video_clip.close()
+
+        # Remove the original video file
+        os.remove(audio_file_path)
+        st.write("Audio downloaded:", current_directory)
+
+def downloader(url):
+    yt = YouTube(url)
+    audio_stream = yt.streams.filter(only_audio=True).first()
+    audio_file_path = audio_stream.download()
+    print("Audio downloaded:", audio_file_path)
+
+    # Convert the downloaded video to audio
+    audio_file_name = os.path.splitext(audio_file_path)[0] + '.mp3'
+    video_clip = VideoFileClip(audio_file_path)
+    audio_clip = video_clip.audio
+    audio_clip.write_audiofile(audio_file_name)
+    audio_clip.close()
+    video_clip.close()
+
+    # Remove the original video file
+    os.remove(audio_file_path)
+
+    print("Audio converted:", audio_file_name)
+
